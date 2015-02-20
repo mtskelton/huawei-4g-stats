083f948c0530f4bcacfda3d05a3047df1afe9206
Mark Skelton <mark@software13.co.uk>
2015-02-20 12:13:26 +0000

    Added filtering on lines

--- stats/views.py
+++ stats/views.py
@@ -11,7 +11,20 @@ class HomeView(TemplateView):
 
 class AjaxDataView(View):
     def get(self, request):
-        data = [['Date / Time', 'Up', 'Down']]
+        show_up = request.GET.get('show_up', 'false') == 'true'
+        show_down = request.GET.get('show_down', 'false') == 'true'
+
+        row = ['Date / Time']
+        if show_up:
+            row.append('Up')
+        if show_down:
+            row.append('Down')
+        data = [row]
         for s in Stat.objects.all().order_by('date'):
-            data.append([s.date.strftime("%Y-%m-%d %H:%M"), s.up, s.down])
+            row = [s.date.strftime("%Y-%m-%d %H:%M")]
+            if show_up:
+                row.append(s.up / 1024)
+            if show_down:
+                row.append(s.down / 1024)
+            data.append(row)
         return HttpResponse(json.dumps(data))
